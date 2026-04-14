2d151ec7b3a38187b152d27ca7d81eba19bb60e3
Eldina Delalić <eldina.maslo@gmail.com>
2026-04-14 15:49:13 +0200

    Vjezbe v6 i v6_v1

---
+++ v6_v1/controllers/student_controller.py
@@ -0,0 +1,36 @@
+from database import SessionDep
+from fastapi import APIRouter
+from schemas.student_schema import CourseInfo, StudentCreate, StudentRead, StudentUpdate
+from services import course_service, student_service
+
+router = APIRouter()
+
+
+@router.post("/", response_model=StudentRead)
+def create_student(student: StudentCreate, session: SessionDep):
+    return student_service.create_student(session, student)
+
+
+@router.get("/", response_model=list[StudentRead])
+def list_students(session: SessionDep, offset: int = 0, limit: int = 100):
+    return student_service.get_students(session, offset, limit)
+
+
+@router.get("/{student_id}", response_model=StudentRead)
+def get_student(student_id: int, session: SessionDep):
+    return student_service.get_student(session, student_id)
+
+
+@router.put("/{student_id}", response_model=StudentRead)
+def update_student(student_id: int, student: StudentUpdate, session: SessionDep):
+    return student_service.update_student(session, student_id, student)
+
+
+@router.delete("/{student_id}")
+def delete_student(student_id: int, session: SessionDep):
+    return student_service.delete_student(session, student_id)
+
+
+@router.get("/{student_id}/courses", response_model=list[CourseInfo])
+def list_student_courses(student_id: int, session: SessionDep):
+    return course_service.get_courses_for_student(session, student_id)
